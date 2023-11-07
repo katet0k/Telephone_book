779786aa61802e5ec28c11320032141f383655a1
Kateryna <kostucenkokata0@gmail.com>
2023-11-07 22:23:35 +0200

    Add project files.

---
+++ ListContact.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+
+namespace Telephone_book
+{
+    public class ListContact : INotifyPropertyChanged
+    {
+       public List<Contact>? contacts = null;
+        private Contact selectedContact;
+
+        public Contact SelectedContact
+        {
+            get { return selectedContact; }
+            set
+            {
+                selectedContact = value;
+                OnPropertyChanged(nameof(Contact));
+            }
+        }
+
+       public ListContact()
+        {
+            contacts = new List<Contact>();
+        }
+
+        public void AddContact(Contact contact)
+        {
+            contacts?.Add(contact);
+        }
+        public void RemoveContact(Contact contact)
+        {
+            contacts?.Remove(contact);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephone_book
+{
+    public class Contact : INotifyPropertyChanged
+    {
+        private string ?_fullName;
+        private string ?_address;
+        private string ?_phoneNumber;
+
+        public string FullName
+        {
+            get { return _fullName; }
+            set
+            {
+                    _fullName = value;
+                    OnPropertyChanged(nameof(FullName));
+                
+            }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                    _address = value;
+                    OnPropertyChanged(nameof(Address));
+                
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set
+            {
+                    _phoneNumber = value;
+                    OnPropertyChanged(nameof(PhoneNumber));
+                
+            }
+        }
+        public Contact(string? fullName, string? address, string? phoneNumber)
+        {
+            FullName = fullName;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
